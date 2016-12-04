bc3099ccfbc840ea43a610a68ba0bf00e2d4ef98
Ari Brenner <abrenner@alumni.conncoll.edu>
2016-12-04 15:55:58 -0500

    stuff

--- heads.js
+++ heads.js
@@ -48,7 +48,8 @@ $(function(){
     var $head = $('<img>')
     left = (left ? left - realSize/2 : getRandomPos('width'))
     top = (top ? top - realSize/2 : getRandomPos('height'))
-    $head.attr('src', 'j.png')
+    var src = window.location.hash.split('#')[1] || 'j.png'
+    $head.attr('src', src)
     $head.data('left', getRandomDir())
     $head.data('top', getRandomDir())
     $head.data('size', size)
@@ -58,6 +59,38 @@ $(function(){
     $head.addClass('js-floating-head floating-head')
     if(flopped) { $head.addClass('flopped') }
     $content.append($head)
+  }
+
+  var negateData = function() {
+    var args = [].slice.call(arguments)
+    $('.js-floating-head').each(function(){
+      var $el = $(this)
+      args.forEach(function(arg) {
+        $el.data(arg, -$el.data(arg))
+      })
+    })
+  }
+
+  var resizeHeads = function(zoomingIn) {
+    // debugger
+    var diff = sizeMult / 10
+    sizeMult += (zoomingIn ? diff : -diff)
+    if (sizeMult > 2) {
+      sizeMult = 2
+    } else if (sizeMult < .25) {
+      sizeMult = .25
+    } else {
+      $('.js-floating-head').each(function(){
+        var $this = $(this)
+        $this.width(sizeMult * $this.data('size'))
+      })
+    }
+  }
+
+  var flipHeads = function () {
+    flopped = !flopped
+    var func = flopped ? 'addClass' : 'removeClass'
+    $('.js-floating-head')[func]('flopped')
   }
 
   for(var i=0; i < 5; i++) { addHead() }
@@ -70,37 +103,25 @@ $(function(){
     }
   })
 
-  $('body').on('keypress', function(e){
+  $('body').on('keydown', function(e){
     var keyCode = e.which
     console.log(keyCode)
     if(keyCode == 32) { // space bar
-      $('.js-floating-head').each(function(){
-        var $this = $(this)
-        $this.data('top', -$this.data('top'))
-        $this.data('left', -$this.data('left'))
-      })
-    } else if (keyCode == 48) {
+      negateData('top', 'left')
+    } else if (keyCode == 48) { //space bar
       paused = !paused
-    } else if (keyCode > 48 && keyCode <= 57){ // 0 to 9
-      var num = keyCode - 48
-      speedMult = Math.pow(num, 2)/10
-    } else if (keyCode == 61 || keyCode == 45) { // +-
-      var diff = sizeMult / 10
-      sizeMult += (keyCode == 61 ? diff : -diff)
-      if (sizeMult > 2) {
-        sizeMult = 2
-      } else if (sizeMult < .25) {
-        sizeMult = .25
-      } else {
-        $('.js-floating-head').each(function(){
-          var $this = $(this)
-          $this.width(sizeMult * $this.data('size'))
-        })
-      }
-    } else if (keyCode == 47 || keyCode == 63) { // ?/
-      flopped = !flopped
-      var func = flopped ? 'addClass' : 'removeClass'
-      $('.js-floating-head')[func]('flopped')
+    } else if (keyCode == 38) { // up arrow
+      if (speedMult < 20) { speedMult *= 1.5 }
+    } else if (keyCode == 40) { // down arrow
+      if (speedMult > .1) { speedMult /= 1.5 }
+    } else if(keyCode == 37) { // left arrow
+      negateData('top')
+    } else if(keyCode == 39) { // right arrow
+      negateData('left')
+    } else if (keyCode == 187 || keyCode == 189) { // +-
+      resizeHeads(keyCode == 187)
+    } else if (keyCode == 191) { // ?/
+      flipHeads()
     } else {
       return
     }
